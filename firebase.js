0a5963759f2bcc4254ef32c441146d74424b81f7
Uche Azubuko <azubukouche@yahoo.com>
2021-11-30 17:42:55 +0000

    Installed firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAlu2uxQPmqB2KJPbX42xqmZhQB6cHw_1U",
+  authDomain: "todo-app-e195a.firebaseapp.com",
+  projectId: "todo-app-e195a",
+  storageBucket: "todo-app-e195a.appspot.com",
+  messagingSenderId: "1075464742482",
+  appId: "1:1075464742482:web:ffb34b1f9e9b8e21d7618b"
+};
+
+const app = initializeApp(firebaseConfig);
